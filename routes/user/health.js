330a0d1e74ec03066090166132adb9e4b32f0b3c
yzhblind <yangzihan2000@outlook.com>
2022-05-30 20:19:37 +0800

    finish health tips

--- routes/user/health.js
+++ routes/user/health.js
@@ -1,9 +1,25 @@
 var router = require('express').Router();
+const passport = require('passport')
 const {announce, department, diagnosis, doctor, order, patient, schedule} = require('../../models');
 
-router.get('/tips', function(req, res, next){
+router.get('/tips', passport.authenticate('jwt', { session: false }), function(req, res, next){
     console.log('health tips request incomes.');
-    res.send('TODO');
+    if (req.user.id == req.query.user_id) {
+        res.json({
+            status: 'success',
+            data:{
+                tips: '一天一个苹果，医生远离我' // 草（x
+            }    
+        })
+    } else {
+        res.status(401).json({
+            status: 'fail',
+            err: {
+                errcode: 106,
+                msg: 'token与请求用户不匹配'
+            }
+        })
+    }
 });
 
 module.exports = router;
