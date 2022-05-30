41651a3d16182e67d4f467049ad2c6c78e9fd58f
yzhblind <yangzihan2000@outlook.com>
2022-05-30 23:32:21 +0800

    finish order info

--- routes/user/order.js
+++ routes/user/order.js
@@ -1,25 +1,63 @@
 var router = require('express').Router();
 const passport = require('passport')
-const {announce, department, diagnosis, doctor, order, patient, schedule} = require('../../models');
+const { announce, department, diagnosis, doctor, order, patient, schedule, order } = require('../../models');
 
-router.get('/query', function(req, res, next){
+router.get('/query', passport.authenticate('jwt', { session: false }), function (req, res, next) {
     console.log('order query request incomes.');
-    res.send('TODO');
+
 });
 
-router.post('/delete', function(req, res, next){
+router.post('/delete', passport.authenticate('jwt', { session: false }), function (req, res, next) {
     console.log('order delete request incomes.');
-    res.send('TODO');
+
 });
 
-router.get('/info', passport.authenticate('jwt', { session: false }), function(req, res, next){
+router.get('/info', passport.authenticate('jwt', { session: false }), function (req, res, next) {
     console.log('order info request incomes.');
-    
+    try {
+        const ord = await order.findById(req.query.order_id).exec()
+        if (ord == null) {
+            return res.json({
+                status: 'fail',
+                err: {
+                    errcode: 107,
+                    msg: '订单不存在'
+                }
+            })
+        }
+        if (req.user.id == ord.user_id) {
+            const user = await patient.findById(req.user.id).exec()
+            const doc = await doctor.findById(ord.doctor_id).exec()
+            res.json({
+                status: 'success',
+                data: {
+                    order_id: ord._id,
+                    user_id: ord.user_id,
+                    doctor_id: ord.doctor_id,
+                    user_name: user.name,
+                    doctor_name: doc.name,
+                    time: ord.time,
+                    status: ord.status,
+                    comments: ord.comments
+                }
+            })
+        } else {
+            res.status(401).json({
+                status: 'fail',
+                err: {
+                    errcode: 106,
+                    msg: 'token与请求用户不匹配'
+                }
+            })
+        }
+    } catch (err) {
+        next(err)
+    }
 });
 
-router.post('/comment', passport.authenticate('jwt', { session: false }), function(req, res, next){
+router.post('/comment', passport.authenticate('jwt', { session: false }), function (req, res, next) {
     console.log('order comment request incomes.');
-    
+
 });
 
 module.exports = router;
