bc9d5b83e3be383b0923b5aa5cfa38d4007bb549
Chen Geng <unrealgengchen@gmail.com>
2022-06-01 00:08:10 +0800

    api: /api/user/schedule/query

--- routes/user/schedule.js
+++ routes/user/schedule.js
@@ -1,9 +1,63 @@
 var router = require('express').Router();
 const {announce, department, diagnosis, doctor, order, patient, schedule} = require('../../models');
+const { cvtDate, cvtTime } = require('../../tools/schedule');
 
-router.post('/query', function(req, res, next){
+router.post('/query', async function(req, res, next){
     console.log('schedule query request incomes.');
-    res.send('TODO');
+    days = req.body.weekdays;
+    departs = req.body.department;
+    sch_doc_dept = await schedule.aggregate([
+        {
+            $lookup: {
+                from: "doctors",
+                localField: 'doctor_id',
+                foreignField: '_id',
+                as: 'doctor_inst'
+            }
+        },
+        {
+            $lookup: {
+                from: "departments",
+                localField: 'doctor_inst.dept_id',
+                foreignField: '_id',
+                as: 'dept_inst'
+            }
+        },
+        {
+            $match: {
+                date: { $in: days },
+                'dept_inst.name': { $in: departs }
+            }
+        }
+    ]);
+    ret = {};
+    for(let i in sch_doc_dept) {
+        sch = sch_doc_dept[i];
+        if(ret[cvtDate(sch.date)] === undefined) {
+            ret[cvtDate(sch.date)] = {
+                [sch.dept_inst[0].name]: {
+                    [cvtTime(sch.time)]: [sch.doctor_inst[0].name]        
+                }
+            }
+        } else {
+            if(ret[cvtDate(sch.date)][sch.dept_inst[0].name] === undefined) {
+                ret[cvtDate(sch.date)][sch.dept_inst[0].name] = {
+                    [cvtTime(sch.time)]: [sch.doctor_inst[0].name]        
+                }
+            } else {
+                if(ret[cvtDate(sch.date)][sch.dept_inst[0].name][cvtTime(sch.time)] === undefined) {
+                    ret[cvtDate(sch.date)][sch.dept_inst[0].name][cvtTime(sch.time)] = [sch.doctor_inst[0].name]
+                } else {
+                    ret[cvtDate(sch.date)][sch.dept_inst[0].name][cvtTime(sch.time)].push(sch.doctor_inst[0].name)
+                }
+            }
+        }
+    }
+    console.log(ret);
+    res.json({
+        status: 'success',
+        data: ret
+    })
 });
 
 module.exports = router;
