1a8137152106369e6c73a485d1875982f3ef0397
yzhblind <yangzihan2000@outlook.com>
2022-06-08 22:15:37 +0800

    add health info set and get

--- models/patient.js
+++ models/patient.js
@@ -2,7 +2,6 @@ const { Schema } = require('mongoose');
 const mongoose = require('./db');
 
 const patientSchema = mongoose.Schema({
-    gender: String,
     name: {
         type: String,
         required: true,
@@ -41,6 +40,48 @@ const patientSchema = mongoose.Schema({
     collect:[],
     pic_id:{
         type:String
+    },
+    health:{
+        total:{
+            value:{
+                type:Number,
+                default: 0
+            },
+            rate:{
+                type:Number,
+                default:0
+            }
+        },
+        pulse_oximeter:{
+            value:{
+                type:Number,
+                default: 0
+            },
+            rate:{
+                type:Number,
+                default: 0
+            }
+        },
+        sleep_quality:{
+            value:{
+                type:Number,
+                default: 0
+            },
+            rate:{
+                type:Number,
+                default: 0
+            }
+        },
+        heart_rate:{
+            value:{
+                type:Number,
+                default: 0
+            },
+            rate:{
+                type:Number,
+                default: 0
+            }
+        }
     }
 });
 
